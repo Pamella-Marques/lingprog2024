e8e5e23bf18e017aa1ebd3ab088053a49687568b
Pamella-Marques <159594591+Pamella-Marques@users.noreply.github.com>
2024-03-09 00:21:50 +0000

    q01 a q10

--- Lista02.c
+++ Lista02.c
@@ -161,11 +161,17 @@ void questao10() {int n1, n2, n3;
 	printf ("Digite três numeros inteiros\n");
 	scanf("%d %d %d", &n1, &n2, &n3);
 	if (n1>n2 && n1>n3 ){
-    if (n2>n3) print ("%f, %f, %f", n1, n2, n3);
-    else print ("%f, %f, %f", n1, n3, n2")}
+    if (n2>n3) printf ("%f, %f, %f", n1, n2, n3);
+    else printf ("%f, %f, %f", n1, n3, n2);}
     else if (n2>n1 && n2>n3){
-    if n1>n3 print ("%f, %f, %f", n2, n1, n3); else print ("%f, %f, %f", n2, n3, n1);}
-    else {if n2>n1 print ("%f, %f, %f", n3, n2, n1); else print ("%f, %f,%f", n3, n1, n2);}}
+    if n1>n3
+        printf ("%f, %f, %f", n2, n1, n3);
+    else if {n2>n1
+        printf ("%f, %f, %f", n3, n2, n1);
+    else if (n3>n1 && n3>n2)
+        printf ("%f, %f, %f", n3, n1, n2);}}
+    else
+        printf ("%f, %f, %f", n2, n3, n1);}
 
 //11. Faça um programa que leia 3 números e imprima o maior deles.
 void questao11() {
